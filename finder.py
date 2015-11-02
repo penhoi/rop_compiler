dea85c15a9c8f2c142b77b2b20ba89b04c19fd18
jeffball <jeffball@dc949.org>
2015-11-01 22:36:06 -0500

    More work on the gadget finder

--- finder.py
+++ finder.py
@@ -6,6 +6,7 @@ import logging
 class Finder(object):
 
   MAX_GADGET_SIZE = 10
+  GADGET_END_INSTRUCTIONS = ['ret']
 
   def __init__(self, filename, base_address = 0, level = logging.WARNING):
     logging.basicConfig(format="%(asctime)s - " + " - %(name)s - %(levelname)s - %(message)s")           
@@ -32,21 +33,27 @@ class Finder(object):
 
   def get_gadgets_for_segment(self, segment):
     disassembler = Cs(CS_ARCH_X86, CS_MODE_64)
+    disassembler.detail = True
     gadgets = []
 
-    for i in range(len(segment.data())):
-      break #TODO finish this
-      for j in range(1, MAX_GADGET_SIZE):
+    data = segment.data()
+    for i in range(1, len(data)):
+      for j in range(1, self.MAX_GADGET_SIZE):
         begin = i - j
         if i - j < 0:
           begin = 0
 
-        code = segment.data[begin:i] 
-        address = self.base_address + segment.p_paddr + begin
-        if self.base_address == 0 and segment.p_paddr == 0:
-          self.logger.WARNING("No base address given for library or PIE executable.  Addresses may be wrong")
-        for inst in md.disasm(code, address):
-          print("0x%x:\t%s\t%s" %(inst.address, inst.mnemonic, inst.op_str))
+        code = data[begin:i] 
+        address = self.base_address + segment.header.p_paddr + begin
+        if self.base_address == 0 and segment.header.p_paddr == 0:
+          self.logger.warning("No base address given for library or PIE executable.  Addresses may be wrong")
+        gadget = [x for x in disassembler.disasm(code, address)] # Expand the generator
+
+        if len(gadget) > 1 and gadget[-1].mnemonic in self.GADGET_END_INSTRUCTIONS:
+          self.logger.debug("Gadget found:")
+          for inst in gadget:
+            self.logger.debug("0x%x:\t%s\t%s", inst.address, inst.mnemonic, inst.op_str)
+          gadgets.append(gadget)
 
     return gadgets
 
@@ -55,7 +62,7 @@ if __name__ == "__main__":
 
   parser = argparse.ArgumentParser(description="Run the gadget locator on the supplied binary") 
   parser.add_argument('target', type=str, help='The file (executable/library) to find gadgets in')
-  parser.add_argument('-base_address', type=str, help='The address the file is loaded at.  Only needed for PIE/PIC binaries')
+  parser.add_argument('-base_address', type=int, default=0, help='The address the file is loaded at.  Only needed for PIE/PIC binaries')
   args = parser.parse_args()
 
   finder = Finder(args.target, args.base_address)
